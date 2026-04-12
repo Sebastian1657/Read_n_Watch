e748fc0911ee94995c02c35b1b51b3251e001edb
Sebastian1657 <miler.sebastian1101@gmail.com>
2026-04-12 20:45:43 +0200

    remove unnecessary node_modules exclusions from settings

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -5,14 +5,14 @@ import { createClient } from "@supabase/supabase-js";
 import { supabaseStorage } from "@/src/api/supabaseStorage";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 
 if (!supabaseUrl) {
   throw new Error("Missing EXPO_PUBLIC_SUPABASE_URL environment variable.");
 }
 
 if (!supabaseAnonKey) {
-  throw new Error("Missing EXPO_PUBLIC_SUPABASE_ANON_KEY environment variable.");
+  throw new Error("Missing EXPO_PUBLIC_SUPABASE_KEY environment variable.");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
